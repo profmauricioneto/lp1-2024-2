de4b2fc0958af4bcc5ba19aec9aaa47e496598e1
profmauricioneto <mauricio.moreira@unichristus.edu.br>
2024-08-27 07:36:46 -0300

    add: matrix exercise

---
+++ java-fundamentals/src/main/java/javafundamentals/MainDiagonal.java
@@ -0,0 +1,23 @@
+package javafundamentals;
+
+import java.util.Scanner;
+
+public class MainDiagonal {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int matrix[][] = new int[3][3];
+        int acc = 0;
+
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                System.out.printf("matrix[%d][%d]: ", i, j);
+                matrix[i][j] = scan.nextInt();
+                // captura os elementos da diagonal principal
+                if (i == j) {
+                    acc = acc + matrix[i][j];
+                }
+            }
+        }
+        System.out.println("Main diagonal Summation: " + acc);
+    }
+}
