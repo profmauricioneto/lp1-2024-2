016d3cf043be9ab5ce0c14e96bad17f6c933f346
profmauricioneto <mauricio.moreira@unichristus.edu.br>
2024-09-03 09:17:52 -0300

    add: file manipulation class

---
+++ java-fundamentals/src/main/java/filemanipulation/CreateFile.java
@@ -0,0 +1,15 @@
+package filemanipulation;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFile {
+    public static void main(String[] args) {
+        File myfile = new File("tx'este.txt");
+        try {
+            myfile.createNewFile();
+        } catch(IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
